01c6735892184c174beb880c5f795237c5c34491
master <gubinvs@gmail.com>
2025-05-27 20:51:08 +0300

    a

--- src/js/ApiUrl.js
+++ src/js/ApiUrl.js
@@ -1,8 +1,8 @@
 
 
 // Переменная с адресом сервера API
-const ApiUrl =  'http://localhost:8080';
-//const ApiUrl =  'https://api.encomponent.ru';
+//const ApiUrl =  'http://localhost:8080';
+const ApiUrl =  'http://31.129.97.48:1030';
 
 
 export default ApiUrl;
