637fb1d26a8a51eb43145664ff3f6d0e148ed6f1
gubinvs@gmail.com <gubinvs@gmail.com>
2025-06-05 12:35:56 +0300

    d

---
+++ src/NavigationBar/NavigationBarMin.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import ApiUrl from '../js/ApiUrl.js';
+import "./navigationBar.css";
+
+
+const NavigationBarMin =()=> {
+    return (
+        <>
+        
+        </>
+    );
+};
+
+export default NavigationBarMin;
