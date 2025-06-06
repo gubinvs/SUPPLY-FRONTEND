e995bafbdcd6c5f2e56ee0ac0f1099c586ddf026
master <gubinvs@gmail.com>
2025-06-06 22:11:23 +0300

    a

--- src/ApplicationPanel/ApplicationPanelRouter.jsx
+++ src/ApplicationPanel/ApplicationPanelRouter.jsx
@@ -1,5 +1,8 @@
-import { useEffect } from "react";
-import { useNavigate, useLocation } from 'react-router-dom';
+// ApplicationPanelRouter.jsx
+// Принимает первое направление при открытии приложения и проверяет роль текущего авторизованного пользователеля, 
+// если пользователь не авторизован перенаправляет его на страницу авторизации
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ROLE_ADMIN = "b5aff5b0-c3ac-4f1e-9467-fe13a14f6de3";
 const ROLE_PROVIDER = "a5219e2b-12f3-490e-99f5-1be54c55cc6d";
@@ -7,45 +10,32 @@ const ROLE_CUSTOMER = "52910536-2b8a-47e7-9d5a-8cca0a0b865a";
 
 const ApplicationPanelRouter = () => {
   const navigate = useNavigate();
-  const location = useLocation();
 
   useEffect(() => {
     const role = localStorage.getItem("roleId");
-    const currentPath = location.pathname;
 
     if (!role) {
-      if (currentPath !== "/AuthorizationForm") {
-        navigate("/AuthorizationForm");
-      }
+      navigate("/AuthorizationForm");
       return;
     }
 
     switch (role) {
       case ROLE_ADMIN:
-        if (currentPath !== "/ApplicationPanelAdmin") {
-          navigate("/ApplicationPanelAdmin");
-        }
+        navigate("/ApplicationPanelAdmin");
         break;
       case ROLE_PROVIDER:
-        if (currentPath !== "/ApplicationPanelProvider") {
-          navigate("/ApplicationPanelProvider");
-        }
+        navigate("/ApplicationPanelProvider");
         break;
       case ROLE_CUSTOMER:
-        if (currentPath !== "/ApplicationPanelCustomer") {
-          navigate("/ApplicationPanelCustomer");
-        }
+        navigate("/ApplicationPanelCustomer");
         break;
       default:
-        if (currentPath !== "/AuthorizationForm") {
-          navigate("/AuthorizationForm");
-        }
+        navigate("/AuthorizationForm");
         break;
     }
-  }, [navigate, location.pathname]); // Только pathname
+  }, [navigate]);
 
-  return null;
+  return null; // Ничего не рендерим, только редиректим
 };
 
 export default ApplicationPanelRouter;
-
