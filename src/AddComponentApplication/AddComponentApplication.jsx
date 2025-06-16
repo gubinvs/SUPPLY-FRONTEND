fbad018da77922c3ee683c58fbe047a9b8d2ca07
gubinvs@gmail.com <gubinvs@gmail.com>
2025-06-16 15:45:15 +0300

    we

--- src/AddComponentApplication/AddComponentApplication.jsx
+++ src/AddComponentApplication/AddComponentApplication.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import "./addComponentApplication.css";
+import ApiUrl from "../js/ApiUrl.js";
 import NavigationBarMin from "../NavigationBar/NavigationBarMin.jsx";
 import NavigationBarMax from "../NavigationBar/NavigationBarMax.jsx";
 import HeaderApplicationPanel from "../ApplicationPanel/Header/HeaderApplicationPanel.jsx";
@@ -12,7 +13,9 @@ const AddComponentApplication = ({ role, title }) => {
     const [isNavMaxVisible, setIsNavMaxVisible] = useState(false);
     const [currentPage, setCurrentPage] = useState(1);
     const [searchTerm, setSearchTerm] = useState("");
-    const [selectedIds, setSelectedIds] = useState(new Set());
+    // const [selectedIds, setSelectedIds] = useState(new Set());
+    const [article, setArticle] = useState("");
+    const [name, setName] = useState('');
 
     useEffect(() => {
         async function loadComponents() {
@@ -46,16 +49,43 @@ const AddComponentApplication = ({ role, title }) => {
         if (currentPage > 1) setCurrentPage((prev) => prev - 1);
     };
 
-    const handleCheckboxToggle = (id) => {
-        setSelectedIds((prev) => {
-            const newSet = new Set(prev);
-            if (newSet.has(id)) {
-                newSet.delete(id);
+    // const handleCheckboxToggle = (id) => {
+    //     setSelectedIds((prev) => {
+    //         const newSet = new Set(prev);
+    //         if (newSet.has(id)) {
+    //             newSet.delete(id);
+    //         } else {
+    //             newSet.add(id);
+    //         }
+    //         return newSet;
+    //     });
+    // };
+
+    const handleAddComponent = async () => {
+        if (!article || !name) {
+            alert('Заполните оба поля');
+            return;
+        }
+
+        try {
+            const response = await fetch(ApiUrl + '/api/AddComponent', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ vendorCodeComponent: article, nameComponent: name })
+            });
+
+            if (response.ok) {
+                alert('Компонент успешно добавлен!');
+                sessionStorage.setItem('lastAddedArticle', article);
+                localStorage.setItem("newArticle", article)
+                window.location.reload();
             } else {
-                newSet.add(id);
+                const errorText = await response.text();
+                alert(`Ошибка при добавлении: ${errorText}`);
             }
-            return newSet;
-        });
+        } catch (error) {
+            alert(`Ошибка соединения: ${error}`);
+        }
     };
 
     return (
@@ -67,12 +97,11 @@ const AddComponentApplication = ({ role, title }) => {
             />
             {isNavMaxVisible && <NavigationBarMax />}
             <HeaderApplicationPanel role={role} title={title} />
-
             <div className="main-application-panel__container">
-                <div className="add-component-application-block">
-                    <h6>Можно проверить наличие в базе</h6>
+                <div className="add-component-application__left-block">
+                    <h6><b>Можно проверить наличие в базе данных:</b></h6>
                     <input
-                        className="form-control"
+                        className="form-control aca-searh-form"
                         type="text"
                         placeholder="Поисковая строка"
                         value={searchTerm}
@@ -87,7 +116,6 @@ const AddComponentApplication = ({ role, title }) => {
                             <table className="table">
                                 <thead className="table-borderless__theder">
                                     <tr>
-                                        <th scope="col" className="table-borderless__check-coll"></th>
                                         <th scope="col" className="table-borderless__article">Артикул</th>
                                         <th scope="col" className="table-borderless__name">Наименование</th>
                                     </tr>
@@ -95,21 +123,13 @@ const AddComponentApplication = ({ role, title }) => {
                                 <tbody className="table-borderless__tbody">
                                     {currentItems.map((item) => (
                                         <tr key={item.id}>
-                                            <td>
-                                                <input
-                                                    className="form-check-input"
-                                                    type="checkbox"
-                                                    checked={selectedIds.has(item.id)}
-                                                    onChange={() => handleCheckboxToggle(item.id)}
-                                                />
-                                            </td>
                                             <td>{item.vendorCodeComponent}</td>
                                             <td>{item.nameComponent}</td>
                                         </tr>
                                     ))}
                                 </tbody>
                             </table>
-                            <div className="pagination-controls__block">
+                            <div className="pagination-controls__block aca-pagination-controls__block">
                                 <div className="pagination-controls">
                                     <button
                                         className="btn btn-outline-primary me-2 pagination-controls__button"
@@ -135,6 +155,28 @@ const AddComponentApplication = ({ role, title }) => {
                     {searchTerm.trim() && filteredItems.length === 0 && (
                         <div className="mt-3 text-muted">Ничего не найдено по запросу.</div>
                     )}
+                </div>
+                <div className="add-component-application__right-block">
+                    <h6><b>Не нашли, добавьте в базу данных:</b></h6>
+                        <div className="add-component-application__input-form">
+                        <input
+                            type="text"
+                            className="form-control aca-input-form__article"
+                            placeholder="Артикул"
+                            value={article}
+                            onChange={(e) => setArticle(e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            className="form-control aca-input-form__name"
+                            placeholder="Наименование"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                        <button className="btn btn-outline-secondary aca-input-form__add-button" type="button" onClick={handleAddComponent}>
+                            Добавить
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
