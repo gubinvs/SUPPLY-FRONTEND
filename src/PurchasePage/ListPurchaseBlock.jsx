6f990159fea79d26e3a53807b7f95ba4948e3566
master <gubinvs@gmail.com>
2025-07-06 20:48:13 +0300

    s

--- src/PurchasePage/ListPurchaseBlock.jsx
+++ src/PurchasePage/ListPurchaseBlock.jsx
@@ -2,69 +2,74 @@ import { useState } from "react";
 import "./listPurchaseBlock.css";
 import ListPurchaseComponent from "./ListPurchaseComponent.jsx";
 
-
-
-const ListPurchaseBlock = (
-    {createPurchase, setCreatePurchase, purchase, purchaseItem}
-) => {
+const ListPurchaseBlock = ({ createPurchase, setCreatePurchase, purchase, purchaseItem }) => {
     const [search, setSearch] = useState('');
-    const [cheked, setCheced] = useState(false);
+    const [checkedPurchaseId, setCheckedPurchaseId] = useState(null);
+
+    const handleCheck = (purchaseId) => {
+        setCheckedPurchaseId(prev =>
+            prev === purchaseId ? null : purchaseId
+        );
+    };
 
     return (
         <>
             <div className="list-purchase-block__form-block">
-                <input 
-                
-                    className={createPurchase?"form-control lp-form-block__input" :"form-control lp-form-block__input mr-10"}
-                    type="text" 
-                    placeholder="Поиск закупки по идентификатору" 
+                <input
+                    className={createPurchase ? "form-control lp-form-block__input" : "form-control lp-form-block__input mr-10"}
+                    type="text"
+                    placeholder="Поиск закупки по идентификатору"
                     aria-label="purchaseId"
                     value={search}
-                    onChange={(e)=>{
+                    onChange={(e) => {
                         setCreatePurchase(false);
                         setSearch(e.target.value);
                     }}
-                />  
-                {!createPurchase?
-                    <button 
-                        type="button" 
-                        className="btn btn-outline-secondary lp-form-block__button" 
-                        onClick={()=>{setCreatePurchase(true)}}
+                />
+                {!createPurchase && (
+                    <button
+                        type="button"
+                        className="btn btn-outline-secondary lp-form-block__button"
+                        onClick={() => setCreatePurchase(true)}
                     >
                         Создать закупку
                     </button>
-                :""}
+                )}
             </div>
+
             <div className="list-purchase-block__list-block">
                 <ul className="list-purchase">
-                    {purchase.map((item)=>{
-                        return(
-                            <>
-                                <li className="list-purchase__item">
-                                    <div className="lp-item__context-block">
-                                        <div className="lp-item__check">
-                                        <input 
-                                            type="checkbox" 
-                                            className="form-check-input"
-                                        />
-                                        </div>
-                                        <div className="lp-item__purchase-id">{item.purchaseId}</div>
-                                        <div className="lp-item__purchase-name">{item.purchaseName}</div>
-                                        <div className="lp-item__purchase-price">{Intl.NumberFormat("ru").format(item.purchasePrice)} р.</div>
-                                        <div className="lp-item__purchase-name-costomer">{item.purchaseCostomer}</div>
-                                    </div>
-                                    <button className="btn btn-outline-warning lp-item__purchase-save">Сохранить изменения</button>
-                                </li>
-                            </>
-                        );
-                    })}
+                    {purchase.map((item) => (
+                        <li key={item.purchaseId} className="list-purchase__item">
+                            <div className="lp-item__context-block">
+                                <div className="lp-item__check">
+                                    <input
+                                        type="checkbox"
+                                        className="form-check-input"
+                                        checked={checkedPurchaseId === item.purchaseId}
+                                        onChange={() => handleCheck(item.purchaseId)}
+                                    />
+                                </div>
+                                <div className="lp-item__purchase-id">{item.purchaseId}</div>
+                                <div className="lp-item__purchase-name">{item.purchaseName}</div>
+                                <div className="lp-item__purchase-price">
+                                    {Intl.NumberFormat("ru").format(item.purchasePrice)} р.
+                                </div>
+                                <div className="lp-item__purchase-name-costomer">{item.purchaseCostomer}</div>
+                            </div>
+                            <button className="btn btn-outline-warning lp-item__purchase-save">
+                                Сохранить изменения
+                            </button>
+                        </li>
+                    ))}
                 </ul>
-                <ListPurchaseComponent 
-                    purchaseItem = {purchaseItem}
-                />
+
+                {/* Показываем компонент, если какая-то закупка выбрана */}
+                {checkedPurchaseId !== null && (
+                    <ListPurchaseComponent purchaseItem={purchaseItem} />
+                )}
             </div>
-            
-        </> 
+        </>
     );
 };
 
