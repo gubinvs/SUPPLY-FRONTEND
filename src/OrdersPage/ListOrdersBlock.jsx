b233552f4d0226582bcce8aa5405ff8da503eba6
gubinvs@gmail.com <gubinvs@gmail.com>
2025-08-07 12:36:54 +0300

    qw

--- src/OrdersPage/ListOrdersBlock.jsx
+++ src/OrdersPage/ListOrdersBlock.jsx
@@ -7,41 +7,42 @@ const ListOrdersBlock  = (
 
     return(
         <>
-            <table className="table">
-                <thead className="table-borderless__theder">
-                    <tr>
-                        <th scope="col"></th>
-                        <th scope="col">Идентификатор</th>
-                        <th scope="col">Наименование</th>
-                        <th scope="col">Стоимость</th>
-                        <th scope="col">Заказчик</th>
-                        <th scope="col">Статус заказа</th>
-                       
-                    </tr>
-                </thead>
-                <tbody>
-                    {ordersList.map((item)=>{
-                        return(
-                            <>
-                              <tr>
-                            <td>
-                                <input
-                                    className="form-check-input"
-                                    type="checkbox"
-                                />
-                            </td>
-                            <td>{item.purchaseId}</td>
-                            <td>{item.purchaseName}</td>
-                            <td>{item.purchasePrice}</td>
-                            <td>{item.purchaseCostomer}</td>
-                            <td>{item.supplyOrderUserStatus }</td>
+            <div className="orders-block-section">
+                <table className="table">
+                    <thead className="table-borderless__theder">
+                        <tr>
+                            <th scope="col"></th>
+                            <th scope="col">Идентификатор</th>
+                            <th scope="col">Наименование</th>
+                            <th scope="col">Стоимость</th>
+                            <th scope="col">Заказчик</th>
+                            <th scope="col">Статус заказа</th>
+                        
                         </tr>
-                            </>
-                        );
-                    })}
-                </tbody>
-            </table>
-        
+                    </thead>
+                    <tbody>
+                        {ordersList.map((item)=>{
+                            return(
+                                <>
+                                <tr>
+                                <td>
+                                    <input
+                                        className="form-check-input"
+                                        type="checkbox"
+                                    />
+                                </td>
+                                <td>{item.purchaseId}</td>
+                                <td>{item.purchaseName}</td>
+                                <td>{item.purchasePrice}</td>
+                                <td>{item.purchaseCostomer}</td>
+                                <td>{item.supplyOrderUserStatus }</td>
+                            </tr>
+                                </>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 
