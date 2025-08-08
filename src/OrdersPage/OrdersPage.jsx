16833237e1dab4d074e43996b44e135ac99831b3
gubinvs@gmail.com <gubinvs@gmail.com>
2025-08-08 11:53:37 +0300

    f

--- src/OrdersPage/OrdersPage.jsx
+++ src/OrdersPage/OrdersPage.jsx
@@ -11,88 +11,87 @@ import ListOrdersBlock from "./ListOrdersBlock.jsx";
 const OrdersPage =(
     {role, title, loading}
 )=> {
-        // Переключатель состояния боковой навигационной панели
-        const [isNavMaxVisible, setIsNavMaxVisible] = useState(false);
-        const handleShowMax = () => setIsNavMaxVisible(true);
-        const handleHideMax = () => setIsNavMaxVisible(false);
 
-        const ordersList = [{
-            guidIdSupplyOrder : "5ae4b8d3-5d4c-4cb0-a664-00ff0322ca1b",
-            guidIdPurchase : "f6837ba1-4974-4880-bdf5-180d584458ae",
-            purchaseId : "ВП25-002",
-            purchaseName : "Шкаф ",
-            purchasePrice : 875180,
-            purchaseCostomer : "ООО \"ААА\"",
-            supplyOrderUserStatus : "новый",
-            orderComponent : [
-                {
-                    vendorCodeComponent : "C40F31M320",
-                    nameComponent : "Выключатель автоматический в литом корпусе NSX400F 36kA 3П3Т 320A MLOGIC 1.3M",
-                    quantityComponent : 4,
-                    priceComponent : 54225,
-                    deliveryTimeComponent : "21-12-2025"
-                },
-                {
-                    vendorCodeComponent : "R500 AI 08 131-000-AAA",
-                    nameComponent : "Модуль аналогового ввода, ТПС/ТЭП, 8 каналов, поканальная гальваническая изоляция, погрешность 0,1%",
-                    quantityComponent : 4,
-                    priceComponent : 372900,
-                    deliveryTimeComponent : "21-12-2025"
-                }
-            ]
-        },{
-            guidIdSupplyOrder : "5ae4b8d3-5d4c-4cb0-a664-00ff0322ca1b",
-            guidIdPurchase : "f6837ba1-4974-4880-bdf5-180d584458ae",
-            purchaseId : "ВП25-002",
-            purchaseName : "Шкаф",
-            purchasePrice : 875180,
-            purchaseCostomer : "ООО \"ААА\"",
-            supplyOrderUserStatus : "новый",
-            orderComponent : [
-                {
-                    vendorCodeComponent : "C40F31M320",
-                    nameComponent : "Выключатель автоматический в литом корпусе NSX400F 36kA 3П3Т 320A MLOGIC 1.3M",
-                    quantityComponent : 4,
-                    priceComponent : 54225,
-                    deliveryTimeComponent : "21-12-2025"
-                },
-                {
-                    vendorCodeComponent : "R500 AI 08 131-000-AAA",
-                    nameComponent : "Модуль аналогового ввода, ТПС/ТЭП, 8 каналов, поканальная гальваническая изоляция, погрешность 0,1%",
-                    quantityComponent : 4,
-                    priceComponent : 372900,
-                    deliveryTimeComponent : "21-12-2025"
-                }
-            ]
-        }];
+    // Переключатель состояния боковой навигационной панели
+    const [isNavMaxVisible, setIsNavMaxVisible] = useState(false);
+    const handleShowMax = () => setIsNavMaxVisible(true);
+    const handleHideMax = () => setIsNavMaxVisible(false);
 
-        return (
-            <>
-                <div className="main-application-panel">
-                    <NavigationBarMin
-                        onShowMax={handleShowMax}
-                        onHideMax={handleHideMax}
-                        isNavMaxVisible={isNavMaxVisible}
+    const ordersList = [{
+        guidIdSupplyOrder : "5ae4b8d3-5d4c-4cb0-a664-00ff0322ca1b",
+        guidIdPurchase : "f6837ba1-4974-4880-bdf5-180d584458ae",
+        purchaseId : "ВП25-002",
+        purchaseName : "Шкаф ",
+        purchasePrice : 875180,
+        purchaseCostomer : "ООО \"ААА\"",
+        supplyOrderUserStatus : "новый",
+        orderComponent : [
+            {
+                vendorCodeComponent : "C40F31M320",
+                nameComponent : "Выключатель автоматический в литом корпусе NSX400F 36kA 3П3Т 320A MLOGIC 1.3M",
+                quantityComponent : 4,
+                priceComponent : 54225,
+                deliveryTimeComponent : "21-12-2025"
+            },
+            {
+                vendorCodeComponent : "R500 AI 08 131-000-AAA",
+                nameComponent : "Модуль аналогового ввода, ТПС/ТЭП, 8 каналов, поканальная гальваническая изоляция, погрешность 0,1%",
+                quantityComponent : 4,
+                priceComponent : 372900,
+                deliveryTimeComponent : "21-12-2025"
+            }
+        ]
+    },{
+        guidIdSupplyOrder : "5ae4b8d3-5d4c-4cb0-a664-00ff0322ca1b",
+        guidIdPurchase : "f6837ba1-4974-4880-bdf5-180d584458ae",
+        purchaseId : "ВП25-002",
+        purchaseName : "Шкаф",
+        purchasePrice : 875180,
+        purchaseCostomer : "ООО \"ААА\"",
+        supplyOrderUserStatus : "новый",
+        orderComponent : [
+            {
+                vendorCodeComponent : "C40F31M320",
+                nameComponent : "Выключатель автоматический в литом корпусе NSX400F 36kA 3П3Т 320A MLOGIC 1.3M",
+                quantityComponent : 4,
+                priceComponent : 54225,
+                deliveryTimeComponent : "21-12-2025"
+            },
+            {
+                vendorCodeComponent : "R500 AI 08 131-000-AAA",
+                nameComponent : "Модуль аналогового ввода, ТПС/ТЭП, 8 каналов, поканальная гальваническая изоляция, погрешность 0,1%",
+                quantityComponent : 4,
+                priceComponent : 372900,
+                deliveryTimeComponent : "21-12-2025"
+            }
+        ]
+    }];
+
+    return (
+        <>
+            <div className="main-application-panel">
+                <NavigationBarMin
+                    onShowMax={handleShowMax}
+                    onHideMax={handleHideMax}
+                    isNavMaxVisible={isNavMaxVisible}
+                />
+
+                {isNavMaxVisible && <NavigationBarMax />}
+                <HeaderApplicationPanel role={role} title={title} /> 
+            </div>
+            <div className="main-application-panel__container">
+                {!loading?        
+                    <ListOrdersBlock 
+                        ordersList={ordersList}
                     />
-
-                    {isNavMaxVisible && <NavigationBarMax />}
-                    <HeaderApplicationPanel role={role} title={title} /> 
-                </div>
-                <div className="main-application-panel__container">
-                    {!loading?        
-                        <ListOrdersBlock 
-                            ordersList={ordersList}
-                        />
-                        :
-                        <div className="orders-page-section">
-                            <Spinners/>
-                        </div>
-                    } 
-                </div>
-            </>
-
-        );
-
+                    :
+                    <div className="orders-page-section">
+                        <Spinners/>
+                    </div>
+                } 
+            </div>
+        </>
+    );
 };
 
 export default OrdersPage;
