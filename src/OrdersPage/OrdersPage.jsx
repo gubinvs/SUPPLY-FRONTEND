a8efe765c9cd5dcacae5a40a695f0b0d1e25bc7f
gubinvs@gmail.com <gubinvs@gmail.com>
2025-08-08 13:50:51 +0300

    wer

--- src/OrdersPage/OrdersPage.jsx
+++ src/OrdersPage/OrdersPage.jsx
@@ -1,5 +1,6 @@
 import "./ordersPage.css";
-import { useState } from "react";
+import ApiUrl from "../js/ApiUrl.js";
+import { useEffect, useState } from "react";
 import Spinners from "../ElementApplication/Spinners.jsx";
 import NavigationBarMin from "../NavigationBar/NavigationBarMin.jsx";
 import NavigationBarMax from "../NavigationBar/NavigationBarMax.jsx";
@@ -15,8 +16,10 @@ const OrdersPage =(
     const [isNavMaxVisible, setIsNavMaxVisible] = useState(false);
     const handleShowMax = () => setIsNavMaxVisible(true);
     const handleHideMax = () => setIsNavMaxVisible(false);
+    const [ordersList, setOrdersList] = useState('');
+    
 
-    const ordersList = [{
+    const orderList = [{
         guidIdSupplyOrder : "5ae4b8d3-5d4c-4cb0-a664-00ff0322ca1b",
         guidIdPurchase : "f6837ba1-4974-4880-bdf5-180d584458ae",
         purchaseId : "ВП25-002",
@@ -65,6 +68,29 @@ const OrdersPage =(
             }
         ]
     }];
+
+    useEffect(()=>{
+        // Запрос api для получения списка доступных заказов на основании 
+            
+                const guidIdCollaborator = localStorage.getItem("guidIdCollaborator");
+
+                fetch(ApiUrl + "/api/ReturnOrderList/" + guidIdCollaborator, {
+                    method: "GET",
+                    headers: { "Content-Type": "application/json" },
+                })
+                    .then((response) => {
+                        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+                        return response.json();
+                    })
+                    .then((data) => {
+                        setOrdersList(data);
+                    })
+                    .catch((error) => {
+                        console.error("Ошибка получения данных:", error);
+                    });
+            
+    },[]);
+
 
     return (
         <>
