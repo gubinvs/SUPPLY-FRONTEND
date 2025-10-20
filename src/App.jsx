d79d67c2fb80db0988ff6c87afd5bc04796c5dfe
master <gubinvs@gmail.com>
2025-10-20 20:54:41 +0300

    as

--- src/App.jsx
+++ src/App.jsx
@@ -66,113 +66,119 @@ const App = () => {
           });
   }, []);
 
-  return (
-    <Router>
-      <Routes>
-        {/* <Route path="/" element={<EditPriceComponent />} /> */}
-        <Route path="/" element={<ApplicationPanelRouter />} />
-        <Route path="/AuthorizationForm" element={<AuthorizationForm />} />
-        <Route path="/Registration" element={<RegistrationForm />} />
-        <Route path="/UpdatePassword" element={<UpdatePassword />} />
-        <Route path='/ExcelPasteInput' element={<ExcelPasteInput />} />
 
-        {/* Роут первоначальный для упрошенного отображения, работает пока не введено новое приложение, потом удалить */}
-        <Route path="/ListComponent" element={
-          <PrivateRoute allowedRoles={[ROLE_ADMIN]}>
-            <ListComponent />
-          </PrivateRoute>} />
+const ROLE_ADMIN = "401694e4-1b16-4b8c-b817-f8a37a4f49dc";
 
 
-        <Route path="/ApplicationPanelAdmin" element={
-          <PrivateRoute allowedRoles={[ROLE_ADMIN]}>
-            <ApplicationPanelAdmin role={role} title="Панель администратора" />
-          </PrivateRoute>
-        } />
 
-        <Route path="/ApplicationPanelProvider" element={
-          <PrivateRoute allowedRoles={[ROLE_PROVIDER, ROLE_ADMIN]}>
-            <ApplicationPanelProvider />
-          </PrivateRoute>
-        } />
 
-        <Route path="/ApplicationPanelCustomer" element={
-          <PrivateRoute allowedRoles={[ROLE_CUSTOMER, ROLE_ADMIN, ROLE_USER]}>
-            <ApplicationPanelCustomer role={role} title={title} />
-          </PrivateRoute>
-        } />
+return (
+  <Router>
+    <Routes>
+      {/* <Route path="/" element={<EditPriceComponent />} /> */}
+      <Route path="/" element={<ApplicationPanelRouter />} />
+      <Route path="/AuthorizationForm" element={<AuthorizationForm />} />
+      <Route path="/Registration" element={<RegistrationForm />} />
+      <Route path="/UpdatePassword" element={<UpdatePassword />} />
+      <Route path='/ExcelPasteInput' element={<ExcelPasteInput />} />
 
-        {/* Страница c формой добавления компонентов в систему */}
-        <Route path="/AddComponentApplication" element={
-          <PrivateRoute allowedRoles={[ROLE_CUSTOMER, ROLE_PROVIDER, ROLE_ADMIN]}>
-            <AddComponentApplication 
-              role={role} 
-              title="Добавление данных"
-              components={components} 
-              
-            />
-          </PrivateRoute>
-        } />
+      {/* Роут первоначальный для упрошенного отображения, работает пока не введено новое приложение, потом удалить */}
+      <Route path="/ListComponent" element={
+        <PrivateRoute allowedRoles={[ROLE_ADMIN]}>
+          <ListComponent />
+        </PrivateRoute>} />
 
-        {/* Страница для заказчика, на ней посковая строка и выдача результатов всех предложеий */}
-        <Route path="/SuppliersOffers" element={
-          <PrivateRoute allowedRoles={[ROLE_CUSTOMER, ROLE_ADMIN, ROLE_USER]}>
-            <SuppliersOffers 
-              role={role} 
-              title="Предложения поставщиков" 
-              components={components}
-              loading={loading}
-              error={error}
-            />
-          </PrivateRoute>
-        } />
 
-        {/* Страница для заказчика, на ней посковая строка и выдача результатов по лучшим предложениям */}
-        <Route path="/AllOffersForSelected" element={
-          <PrivateRoute allowedRoles={[ROLE_CUSTOMER, ROLE_ADMIN, ROLE_USER]}>
-            <AllOffersForSelected role={role} title="Предложения по выбранным артикулам" />
-          </PrivateRoute>
-        } />
+      <Route path="/ApplicationPanelAdmin" element={
+        <PrivateRoute allowedRoles={[ROLE_ADMIN]}>
+          <ApplicationPanelAdmin role={role} title="Панель администратора" />
+        </PrivateRoute>
+      } />
 
-        {/* Страница для Редактирование номенклатуры в приложении, корректировка артикулов и наименований */}
-        <Route path="/EditSupplyComponent" element={
-          <PrivateRoute allowedRoles={[ROLE_CUSTOMER, ROLE_ADMIN, ROLE_PROVIDER]}>
-            <EditSupplyComponent 
-              role={role} 
-              title="Редактирование номенклатуры"
-              components={components}
-              error={error}
-            />
-          </PrivateRoute>
-        } />
+      <Route path="/ApplicationPanelProvider" element={
+        <PrivateRoute allowedRoles={[ROLE_PROVIDER, ROLE_ADMIN]}>
+          <ApplicationPanelProvider />
+        </PrivateRoute>
+      } />
 
-        {/* Страница c данными о спецификациях */}
-          <Route path="/Purchase" element={
-          <PrivateRoute allowedRoles={[ROLE_CUSTOMER, ROLE_ADMIN, ROLE_USER]}>
-            <PurchasePage
-              role={role} 
-              title="Спецификации пользователя"
-              components={components}
-              profitability={profitability}
-              error={error}
-              loading={loading}
-            />
-          </PrivateRoute>
-        } />
+      <Route path="/ApplicationPanelCustomer" element={
+        <PrivateRoute allowedRoles={[ROLE_CUSTOMER, ROLE_ADMIN, ROLE_USER]}>
+          <ApplicationPanelCustomer role={role} title={title} />
+        </PrivateRoute>
+      } />
 
-        {/* Страница c данными о заказах (закупке) */}
-          <Route path="/OrdersPage" element={
-          <PrivateRoute allowedRoles={[ROLE_CUSTOMER, ROLE_ADMIN, ROLE_USER]}>
-            <OrdersPage 
-              role={role} 
-              title="Заказы пользователя"
-              loading={loading}
-            />
-          </PrivateRoute>
-        } />
+      {/* Страница c формой добавления компонентов в систему */}
+      <Route path="/AddComponentApplication" element={
+        <PrivateRoute allowedRoles={[ROLE_CUSTOMER, ROLE_PROVIDER, ROLE_ADMIN]}>
+          <AddComponentApplication 
+            role={role} 
+            title="Добавление данных"
+            components={components} 
+            
+          />
+        </PrivateRoute>
+      } />
 
-      </Routes>
-    </Router>
-  );
+      {/* Страница для заказчика, на ней посковая строка и выдача результатов всех предложеий */}
+      <Route path="/SuppliersOffers" element={
+        <PrivateRoute allowedRoles={[ROLE_CUSTOMER, ROLE_ADMIN, ROLE_USER]}>
+          <SuppliersOffers 
+            role={role} 
+            title="Предложения поставщиков" 
+            components={components}
+            loading={loading}
+            error={error}
+          />
+        </PrivateRoute>
+      } />
+
+      {/* Страница для заказчика, на ней посковая строка и выдача результатов по лучшим предложениям */}
+      <Route path="/AllOffersForSelected" element={
+        <PrivateRoute allowedRoles={[ROLE_CUSTOMER, ROLE_ADMIN, ROLE_USER]}>
+          <AllOffersForSelected role={role} title="Предложения по выбранным артикулам" />
+        </PrivateRoute>
+      } />
+
+      {/* Страница для Редактирование номенклатуры в приложении, корректировка артикулов и наименований */}
+      <Route path="/EditSupplyComponent" element={
+        <PrivateRoute allowedRoles={[ROLE_CUSTOMER, ROLE_ADMIN, ROLE_PROVIDER]}>
+          <EditSupplyComponent 
+            role={role} 
+            title="Редактирование номенклатуры"
+            components={components}
+            error={error}
+          />
+        </PrivateRoute>
+      } />
+
+      {/* Страница c данными о спецификациях */}
+        <Route path="/Purchase" element={
+        <PrivateRoute allowedRoles={[ROLE_CUSTOMER, ROLE_ADMIN, ROLE_USER]}>
+          <PurchasePage
+            role={role} 
+            title="Спецификации пользователя"
+            components={components}
+            profitability={profitability}
+            error={error}
+            loading={loading}
+          />
+        </PrivateRoute>
+      } />
+
+      {/* Страница c данными о заказах (закупке) */}
+        <Route path="/OrdersPage" element={
+        <PrivateRoute allowedRoles={[ROLE_CUSTOMER, ROLE_ADMIN, ROLE_USER]}>
+          <OrdersPage 
+            role={role} 
+            title="Заказы пользователя"
+            loading={loading}
+          />
+        </PrivateRoute>
+      } />
+
+    </Routes>
+  </Router>
+);
 };
 
 export default App;
