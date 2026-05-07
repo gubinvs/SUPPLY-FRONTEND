6290cfaeb9685f9c0e3a18a32628ce6e56e25137
gubinvs@gmail.com <gubinvs@gmail.com>
2026-05-07 13:21:53 +0300

    q

--- src/js/ApiUrl.js
+++ src/js/ApiUrl.js
@@ -2,9 +2,10 @@
 
 // Переменная с адресом сервера API
 
-const ApiUrl =  'http://127.0.0.1:1030'; 
+// const ApiUrl =  'http://127.0.0.1:1030'; 
 // const ApiUrl =  "https://supplyapi.encomponent.ru";
 // const ApiUrl =  'http://31.129.97.48:1030';
+const ApiUrl =  '/api'; 
 
 
 
