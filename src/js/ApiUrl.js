b0e2c7a4008aeaf23dca0305fc127c3768f258b9
gubinvs@gmail.com <gubinvs@gmail.com>
2026-05-29 10:55:28 +0300

    e

--- src/js/ApiUrl.js
+++ src/js/ApiUrl.js
@@ -2,8 +2,8 @@
 
 // Переменная с адресом сервера API
 
-const ApiUrl =  'http://localhost:8080'; 
-// const ApiUrl =  'http://192.168.1.43:1040/api';
+// const ApiUrl =  'http://localhost:8080'; 
+const ApiUrl =  'http://192.168.1.43:1040/api';
 // const ApiUrl =  '/api'; 
 
 
