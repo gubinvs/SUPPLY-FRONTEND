f117ddf95f066e9c4b58cf92b475ed0bbb3b1e09
gubinvs@gmail.com <gubinvs@gmail.com>
2026-06-02 15:37:41 +0300

    we

--- src/SuppliersOffers/AllOffersForSelected.jsx
+++ src/SuppliersOffers/AllOffersForSelected.jsx
@@ -306,6 +306,7 @@ const AllOffersForSelected = (
                         <a 
                           href={"http://192.168.1.43"} 
                           target="_blank"
+                          
                         >
                             {offer.nameProvider}
                         </a>
